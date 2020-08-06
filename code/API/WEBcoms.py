eec065ddd5e7d77f6978039d6acae81dab2ebd2d
Mathilde Tannebæk <s184376@student.dtu.dk>
2020-08-06 10:03:47 +0200

    Update WEBcoms.py

--- code/API/WEBcoms.py
+++ code/API/WEBcoms.py
@@ -13,3 +13,21 @@ wifi.connect(internetName,internetCode)
 
 #ap_if = network.WLAN(network-AP_IF)
 #ap_if.active(False)
+
+
+
+#Here is the test for wether you are connected to the wifi
+#If you are connected, the first 1000 characters received 
+#from the www.dtu.dk web-page are printed
+
+import socket
+
+request = b"GET / HTTP/1.1\nHost: www.dtu.dk\n\n" 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.dtu.dk", 80))
+s.settimeout(2) 
+s.send(request) 
+result = s.recv(10000) 
+print(result) 
+s.close()
+
