b294625f91bc9c14d9769e0c97ef245aaf02a59f
Barrikad <s184459@student.dtu.dk>
2020-08-07 13:51:40 +0200

    made a flag clock

---
+++ code/HAL/temperature_sensor.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  7 13:17:30 2020
+
+@author: simon
+"""
+
+class TemperatureSensor:
+    def get_temperature(self):
+        pass
--- tests/test_cooling_api.py
+++ tests/test_cooling_api.py
@@ -10,9 +10,9 @@ import code.API.cooling_api as ca
 class TestCoolingAPI(ut.TestCase):
     
     def setUp(self):
-        self.coolingAPI = ca.CoolingAPI()
         self.temperatureSensor = EmulatedTemperatureSensor()
         self.relay = EmulatedRelay()
+        self.coolingAPI = ca.CoolingAPI(self.temperatureSensor, self.relay)
     
     def tearDown(self):
         self.coolingAPI = None
@@ -21,13 +21,32 @@ class TestCoolingAPI(ut.TestCase):
         self.assertIsNotNone(self.coolingAPI)
     
     def test_turn_on_intense_cooling(self):
-        self.coolingAPI.turn_on
+        self.coolingAPI.intense_cooling(True)
+        self.assertTrue(self.relay.fan_value)
+    
+    def test_turn_off_intense_cooling(self):
+        self.coolingAPI.intense_cooling(True)
+        self.coolingAPI.intense_cooling(False)
+        self.assertFalse(self.relay.fan_value)
+    
+    def test_get_current_temperature(self):
+        self.temperatureSensor.temperature = 15
+        self.assertEqual(self.coolingAPI.get_current_temperature(),15)
     
 
 class EmulatedTemperatureSensor:
     def __init__(self):
-        pass
+        self.temperature = 20
+    
+    def get_temperature(self):
+        return self.temperature
     
 class EmulatedRelay:
     def __init__(self):
-        pass
+        self.fan_value = False
+    
+    def set_fan_high(self):
+        self.fan_value = True
+    
+    def set_fan_low(self):
+        self.fan_value = False
