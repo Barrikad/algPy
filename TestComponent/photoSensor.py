2537e2329553dfa611c83c2e4d9d814577c4e20c
Mathilde Tannebæk <s184376@student.dtu.dk>
2020-08-07 10:22:04 +0200

    photosensor

---
+++ TestComponent/photoSensor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  7 10:11:07 2020
+
+@author: mathildetannebaek
+"""
+
+#We should get different readings from the photosensor when 
+#exposing it against different light. 10 readings per sec are made
+
+#Note: i'm not sure if it prints the reading, else we need to insert that
+
+#insert adc pin here:
+adcPin = 32
+
+
+import machine
+import time
+
+adc = machine.ADC(machine.Pin(adcPin))
+adc.atten(machine.ADC.ATTN_11DB)
+
+while True:
+    for i in range(1024):
+        time.sleep(0.1)
+        adc.read()
