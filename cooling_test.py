1b0028247599367dbe09e620fd5dd6eef96d737d
NajaJean <naja-jean@live.dk>
2020-08-12 10:29:03 +0200

    yes

--- cooling_test.py
+++ cooling_test.py
@@ -1,16 +1,18 @@
-from code.HAL.temperature_sensor import get_temperature
-import oled
+import code.HAL.temperature_sensor as temp
+import code.HAL.oled as oled
 import time
 
 tempData = []
-display = Oled()
+display = oled.Oled()
 
-for i in range (200)
-    value = get_temperature()
+tempreader = temp.TemperatureSensor(32)
+
+for i in range(200):
+    value = tempreader.get_temperature()
     tempData.append(value)
     print(value)
-    display.write_to_oled("temperature: ",value," ")
-    time.sleep(15)
+    display.write_to_oled("temperature: ",str(value),"...")
+    time.sleep(5)
 
 print("-----------------")
 print(tempData)
