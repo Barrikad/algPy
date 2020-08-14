5359e1c8ff71d83c2b1dd1959089d72369aab8f8
Barrikad <s184459@student.dtu.dk>
2020-08-14 07:17:11 +0200

    algea and od fix

--- code/API/feeding_api.py
+++ code/API/feeding_api.py
@@ -10,6 +10,7 @@ class FeedingAPI:
         self.algeaSensor = algeaSensor
         self.pump = pump
         self.pumpedVolume = 0
+        self.algaeSum = 0
     
     def get_current_algea_density(self):
         """algea per ml
@@ -18,14 +19,20 @@ class FeedingAPI:
     
     def start_feeding(self):
         self.pump.start_pump()
+        self.algaeSum = 0
+        self.pumpedVolume = 0
+        
         
     def stop_feeding(self):
         self.pumpedVolume = self.pump.get_pumped_volume()
         self.pump.stop_pump()
     
     def total_fed_algea(self):
-        algae_density = 1800000 #self.algeaSensor.get_density() 
-        return algae_density * self.pump.get_pumped_volume()
+        algae_density = 1800000 #self.algeaSensor.get_density()
+        pVolume = self.pump.get_pumped_volume
+        self.algaeSum += (pVolume - self.pumpedVolume) * algae_density
+        self.pumpedVolume = pVolume
+        return self.algaeSum
 
     def start_back_water(self):
         self.pump.reverse_direction()
